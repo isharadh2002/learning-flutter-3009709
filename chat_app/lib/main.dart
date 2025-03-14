a71df6c249ee1c48ebe87538e09677be54be402f
Ishara Dhanushan <109136246+isharadh2002@users.noreply.github.com>
2025-03-14 20:52:51 +0530

    Making changes

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -13,33 +13,9 @@ class ChatApp extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
         title: "Flutter Chat App",
-        theme: ThemeData(primarySwatch: Colors.yellow),
+        theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)),
         themeMode: ThemeMode.system,
-        home: const LoginPage());
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Chat App"),
-        backgroundColor: Colors.blue,
-      ),
-      drawer: const Drawer(
-        backgroundColor: Colors.lightBlueAccent,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print("Button Pressed");
-        },
-        backgroundColor: Colors.blue,
-        child: const Icon(Icons.add),
-      ),
-      body: const Text("Hello"),
+        home: const LoginPage()
     );
   }
 }
